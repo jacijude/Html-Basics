255468c5ad11b84811b73f30f2ee2783d9c0eab2
Jacintha Jude <jacijude@gmail.com>
2022-12-14 21:41:28 -0500

    js-basics02.js

--- JS-basics/js-basics01.js
+++ JS-basics/js-basics01.js
@@ -1,12 +1,16 @@
 let district = 
 {
-Name: 'Asher',
-Library: 'Dom Library, Asher library',
-Years: 50,
-Attraction: 'Asher lake'
+name: 'Asher',
+library: 'Dom Library, Asher library',
+yearsOld: 50,
+attraction: 'Asher lake, Dom Lake, Tinton Falls'
 }
-console.log(district.Name)
-console.log(district.Library)
-console.log(district.Years)
-console.log(district.Attraction)
-console.log(district.Library(1))
+console.log("District: ")
+console.log(district.name)
+console.log("Library: ")
+console.log(district.library)
+console.log("Built around: ")
+console.log(district.yearsOld)
+console.log("Famous Attractions: ")
+console.log(district.attraction)
+console.log(district.name, district.yearsOld)
